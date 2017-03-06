178a3cf35903350e1340207b108372e78d171352
marshallcomm <marshallcomm@163.com>
2017-03-06 13:38:46 +0800

    Site updated: 2017-03-06 13:38:45

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 06 2017 10:54:50 GMT+0800 (中国标准时间)
+// build time:Mon Mar 06 2017 13:38:28 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
