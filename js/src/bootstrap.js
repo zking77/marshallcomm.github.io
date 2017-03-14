6fc48039a333bd78fdca1dec035609d67711435b
marshallcomm <marshallcomm@163.com>
2017-03-14 08:30:15 +0800

    Site updated: 2017-03-14 08:30:08

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 13 2017 23:03:53 GMT+0800 (中国标准时间)
+// build time:Tue Mar 14 2017 08:29:56 GMT+0800 (中国标准时间)
 $(document).ready(function(){$(document).trigger("bootstrap:before");NexT.utils.isMobile()&&window.FastClick.attach(document.body);NexT.utils.lazyLoadPostsImages();NexT.utils.registerBackToTop();$(".site-nav-toggle button").on("click",function(){var e=$(".site-nav");var o="site-nav-on";var t=e.hasClass(o);var a=t?"slideUp":"slideDown";var i=t?"removeClass":"addClass";e.stop()[a]("fast",function(){e[i](o)})});CONFIG.fancybox&&NexT.utils.wrapImageWithFancyBox();NexT.utils.embeddedVideoTransformer();NexT.utils.addActiveClassToMenuItem();NexT.motion.integrator.add(NexT.motion.middleWares.logo).add(NexT.motion.middleWares.menu).add(NexT.motion.middleWares.postList).add(NexT.motion.middleWares.sidebar);$(document).trigger("motion:before");CONFIG.motion&&NexT.motion.integrator.bootstrap();$(document).trigger("bootstrap:after")});
 //rebuild by neat 
