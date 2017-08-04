ea84b299a2a7f6f5cfe80756c4cd7c2a1590e525
marshallcomm <marshallcomm@163.com>
2017-08-04 16:47:06 +0800

    Site updated: 2017-08-04 16:46:53

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 02 2017 10:37:49 GMT+0800 (中国标准时间)
+// build time:Fri Aug 04 2017 16:44:42 GMT+0800 (中国标准时间)
 $(document).ready(function(){$(document).trigger("bootstrap:before");NexT.utils.isMobile()&&window.FastClick.attach(document.body);NexT.utils.lazyLoadPostsImages();NexT.utils.registerBackToTop();$(".site-nav-toggle button").on("click",function(){var e=$(".site-nav");var o="site-nav-on";var t=e.hasClass(o);var a=t?"slideUp":"slideDown";var i=t?"removeClass":"addClass";e.stop()[a]("fast",function(){e[i](o)})});CONFIG.fancybox&&NexT.utils.wrapImageWithFancyBox();NexT.utils.embeddedVideoTransformer();NexT.utils.addActiveClassToMenuItem();NexT.motion.integrator.add(NexT.motion.middleWares.logo).add(NexT.motion.middleWares.menu).add(NexT.motion.middleWares.postList).add(NexT.motion.middleWares.sidebar);$(document).trigger("motion:before");CONFIG.motion&&NexT.motion.integrator.bootstrap();$(document).trigger("bootstrap:after")});
 //rebuild by neat 
