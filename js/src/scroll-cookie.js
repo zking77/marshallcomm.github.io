84e45d261bc5e92e2f9b79730e110444779879c9
marshallcomm <marshallcomm@163.com>
2017-10-20 11:40:26 +0800

    Site updated: 2017-10-20 11:40:07

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Sep 19 2017 20:03:28 GMT+0800 (中国标准时间)
+// build time:Fri Oct 20 2017 11:39:46 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
