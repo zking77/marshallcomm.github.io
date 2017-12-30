2df8420a37d3c5313a586b8e4262ebb4f746e7d2
marshallcomm <marshallcomm@163.com>
2017-12-30 10:52:33 +0800

    Site updated: 2017-12-30 10:52:29

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 30 2017 10:45:14 GMT+0800 (中国标准时间)
+// build time:Sat Dec 30 2017 10:52:11 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
